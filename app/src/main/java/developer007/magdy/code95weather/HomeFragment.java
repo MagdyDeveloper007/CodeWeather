f6a078a40f2e50b57dd2686b05c3c4718ea29e81
MagdyDeveloper007 <67636789+MagdyDeveloper007@users.noreply.github.com>
2021-09-14 23:33:38 +0200

    Updating UI

--- app/src/main/java/developer007/magdy/code95weather/HomeFragment.java
+++ app/src/main/java/developer007/magdy/code95weather/HomeFragment.java
@@ -4,23 +4,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import developer007.magdy.code95weather.data.SharedPrefManager;
+import pl.droidsonroids.gif.GifImageView;
 
 public class HomeFragment extends Fragment {
+    private TextView tvTodayMetric, tvTodayImperial, tvTodayDegree, tvTodayDesc,
+            tvTodayDate, tvTodayFeelLike, tvTodayHum, tvTodayWind, tvTodayMinMax;
+    private EditText etLocation;
+    private ImageButton imageLocation;
+    private GifImageView progress;
+    private ImageView imgToday;
+    private RecyclerView recyclerTime;
+    private String strTodayUnit, strTodayDegree, strTodayDesc,
+            strLocation, strAppId, strIconBase, strIconExt, strImageIcon, strSpeed, strMinMax;
+    private long strTodayDate;
+    private String feels_like, temp_min, temp_max, humidity;
+    private SharedPrefManager sharedPrefManager;
+    //todo private WeatherViewModel weatherViewModel;
+    private AppCompatActivity compatActivity;
+    private static final String TAG = "TodayFragment";
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        compatActivity = (AppCompatActivity) view.getContext();
+        return view;
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+/*    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
@@ -30,5 +55,5 @@ public class HomeFragment extends Fragment {
                         .navigate(R.id.action_HomeFragment_to_SettingFragment);
             }
         });
-    }
+    }*/
 }
